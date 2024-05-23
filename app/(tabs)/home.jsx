c3aadf56b967c62590edbc54ca229bbc77902862
Grace <graceadegunle16@gmail.com>
2024-05-23 20:39:34 +0100

    commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React from "react";
+import { images } from "../../constants";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
 
-const Home = () => {
+const Header = () => {
   return (
     <View>
-      <Text>Welcome Back</Text>
-      <Text>jsmastery</Text>
+      <View>
+        <Text className=" font-pmedium text-sm text-gray-100">
+          Welcome Back
+        </Text>
+        <Text className="text-2xl font-psemibold text-white">JSMastery</Text>
+      </View>
+      <View className="mt-1.5">
+        <Image
+          source={images.logoSmall}
+          className="w-9 h-10"
+          resizeMode="contain"
+        />
+      </View>
+      <SearchInput placeholder={"Search for a video topic"} />
+      <View className="w-full flex-1 pt-5 pb-8">
+        <Text className="text-gray-100 text-lg font-pregular mb-3">
+          Trending Videos
+        </Text>
+        <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+        the double
+      </View>
     </View>
+  );
+};
+const Home = () => {
+  return (
+    <SafeAreaView className=" bg-primary">
+      <FlatListquestion
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <Text className="text-3xl">{item.id} </Text>}
+        ListHeaderComponent={Header}
+      />
+    </SafeAreaView>
   );
 };
 
